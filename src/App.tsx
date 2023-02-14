9f79b472ff3753ce72d3f262a82106a3747b5997
mateuscatao <mateuscatao1311@gmail.com>
2023-02-14 20:22:29 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import { Header } from './components/Header';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css'
+
+import './global.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/mateuscatao.png",
+      name: "Mateus Catão",
+      role: "Front End Developer"
+    },
+    content: [
+      { type: "paragraph", content: "Novo post"},
+      { type: "paragraph", content: "Testando parágrafos"},
+      { type: "link", content: "github.com/mateuscatao"},         
+    ],
+    publishedAt: new Date("2023-01-31 15:24:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/rafaelpapastamatiou.png",
+      name: "Rafael Papastamatiou",
+      role: "Full Stack Senior Developer"
+    },
+    content: [
+      { type: "paragraph", content: "Mais um post"},
+      { type: "paragraph", content: "testando mais parágrafos"},
+      { type: "link", content: "github.com/rafaelpapastamatiou"},         
+    ],
+    publishedAt: new Date("2023-01-30 10:00:00"),
+  },
+];
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Comment.tsx
@@ -0,0 +1,57 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import { useState } from 'react';
+import { Avatar } from './Avatar'
+import styles from './Comment.module.css'
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikedComment() {
+    setLikeCount((state) => {
+      return state + 1
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar 
+        hasBorder={false} 
+        src="https://avatars.githubusercontent.com/u/51727745?v=4" 
+        alt="" 
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Mateus Catão</strong>
+              <time title='20 de Janeiro às 17:56h' dateTime='2023-01-20'>Cerca de 2h atrás</time>
+            </div>
+
+            <button onClick={handleDeleteComment} title='Deletar cometário'>
+              <Trash size={24}/>
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikedComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
